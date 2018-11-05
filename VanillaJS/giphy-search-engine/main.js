8ef35eb5f3f9a90e5d898fe8e2736bac38c3b2fd
billy <sangyounw@naver.com>
2018-11-05 14:48:44 +0900

    set hiphy API

--- VanillaJS/giphy-search-engine/main.js
+++ VanillaJS/giphy-search-engine/main.js
@@ -14,11 +14,14 @@ inputArea.addEventListener('keyup', (e) => {
     }
 });
 
-// 2. API 를 활용하여 data를 받는다. 그리고 가공한다
+// 2. API 를 활용하여 data를 받는다. 그리고 가공한다.
+    const API_KEY = '5JeB48DMaF1D3h29EzgcdwCQMqLJxnNY';
+    let keyword = 'cat';
+    const URL = `api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${API_KEY}`;
+    console.log(URL);
 
 // 3. GIF 파일들을 index.html에 밀어 넣는다.
 const pushToDOM = (data) => {
     const resultArea = document.querySelector("#result-area");
     resultArea.innerHTML = data;
- 
 }
