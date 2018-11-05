a6979bf5f374e0677631226c8db3bfbc8a7caa43
billy <sangyounw@naver.com>
2018-11-05 17:18:20 +0900

    complete giphy search engine with TV

--- VanillaJS/giphy-search-engine/main.js
+++ VanillaJS/giphy-search-engine/main.js
@@ -1,23 +1,25 @@
 // 1. <input> 태그 안의 값을 잡는다
 const button = document.querySelector('#js-button');
 const inputArea = document.querySelector('#js-input');
+const resultArea = document.querySelector("#result-area");
 
 button.addEventListener('click', () => {
     const inputValue = document.querySelector('#js-input').value;
-    pushToDOM(inputValue);
+    searchAndPush(inputValue);
 });
 
 inputArea.addEventListener('keyup', (e) => {
     const keyValue = document.querySelector('#js-input').value;
     if(e.which === 13){
-     pushToDOM(keyValue);
+        searchAndPush(keyValue);
     }
 });
 
+const searchAndPush = (keyword) => {
 // 2. API 를 활용하여 data를 받는다. 그리고 가공한다.
     const API_KEY = '5JeB48DMaF1D3h29EzgcdwCQMqLJxnNY';
-    let keyword = 'turtle';
     const URL = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${API_KEY}`;
+    console.log(URL);
     
     // Ajax request
     const GiphyAJAXCall = new XMLHttpRequest();
@@ -27,22 +29,23 @@ inputArea.addEventListener('keyup', (e) => {
     GiphyAJAXCall.addEventListener('load', (e) => {
         const rawData = e.target.response;
         const parsedData = JSON.parse(rawData);
-        pushToDOM(parsedData);
-        
+        console.log(parsedData);
+        pushToDOM(parsedData);    
     });
-
+};
 
 // 3. GIF 파일들을 index.html에 밀어 넣는다.
 const pushToDOM = (parsedData) => {
-    const resultArea = document.querySelector("#result-area");
+    resultArea.innerHTML = null;
+    console.log(parsedData);
     const DataSet = parsedData.data;
     // console.log(DataSet);
 
     // iterator
     DataSet.forEach((imageData) => {
         let imageURL = imageData.images.fixed_height.url;
-        let imageALT = imageData.title;
-        resultArea.innerHTML += `<img src="${imageURL}" alt="${imageALT}"/>`;
+        let alt = imageData.title;
+        resultArea.innerHTML += `<img src="${imageURL}" alt='${alt}'/>`;
     });
 
     // let imageURL = parsedData.data[1].images.fixed_height.url;
