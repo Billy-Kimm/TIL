759912bc4344fda5c698f4430d0b4e3d23773227
billy <sangyounw@naver.com>
2018-11-19 16:51:59 +0900

    add segment

--- react/components/src/index.js
+++ react/components/src/index.js
@@ -1,44 +1,50 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
-import Message from './Message';
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import Message from "./Message";
+import Segment from "./Segment";
 
 const App = () => {
   return (
     <Fragment>
-    <Message header="안녕하세요" body="어서오십숑"/>
+      <Segment>
+        <div className="ui icon header">
+          <i className="pdf file outline icon">No Document</i>
+        </div>
+        
+        <div className="ui primary button">Add document</div>
+      </Segment>
+
+      <Segment>
+        <h4 className="ui header">
+          For Your Infromation!
+        </h4>
+        <p>
+          {faker.lorem.paragraph()}
+        </p>
+      </Segment>
+
+      <Message header="회원 약관 변경" body="약관이 변경되었습니다. 동의하실거죠?" />
+      
       <div className="ui container comments">
+        
         <ApprovalCard>
-            <CommentDetail 
-                author={faker.name.firstName()} 
-                time={faker.date.recent().toLocaleString()}
-                body={faker.lorem.sentence()}
-                avatar={faker.image.avatar()}
-                />
-        </ApprovalCard> 
- 
-       <ApprovalCard>
-            <CommentDetail 
-                author={faker.name.firstName()} 
-                time={faker.date.recent().toLocaleString()}
-                body={faker.lorem.sentence()}
-                avatar={faker.image.avatar()}
-                />
+          <h4>오늘 새벽까지 복습한다.</h4>
+          <p>빡공각?</p>
         </ApprovalCard>
 
         <ApprovalCard>
-            <CommentDetail 
-                author={faker.name.firstName()} 
-                time={faker.date.recent().toLocaleString()}
-                body={faker.lorem.sentence()}
-                avatar={faker.image.avatar()}
-                />
-        </ApprovalCard>    
-    </div>
+          <CommentDetail
+            author={faker.name.firstName()}
+            time={faker.date.recent().toLocaleString()}
+            body={faker.lorem.sentence()}
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+      </div>
     </Fragment>
-
   );
 };
 
