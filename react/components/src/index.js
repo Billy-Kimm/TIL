cd095fb5bf62a6110811aa33e85780443298e7ab
billy <sangyounw@naver.com>
2018-11-19 16:38:33 +0900

    Learn Components Fragment

--- react/components/src/index.js
+++ react/components/src/index.js
@@ -1,13 +1,15 @@
-//import React, { Component } from 'react';
-import React from "react";
+import React, { Fragment } from 'react';
 import ReactDOM from "react-dom";
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
+import Message from './Message';
 
 const App = () => {
   return (
-    <div className="ui container comments">
+    <Fragment>
+    <Message header="안녕하세요" body="어서오십숑"/>
+      <div className="ui container comments">
         <ApprovalCard>
             <CommentDetail 
                 author={faker.name.firstName()} 
@@ -35,6 +37,8 @@ const App = () => {
                 />
         </ApprovalCard>    
     </div>
+    </Fragment>
+
   );
 };
 
