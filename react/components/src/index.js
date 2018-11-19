1d8f302f9cdbae95df1bbca733dd97b78b00a2dd
billy <sangyounw@naver.com>
2018-11-19 15:50:02 +0900

    Learn React Components

---
+++ react/components/src/index.js
@@ -0,0 +1,32 @@
+//import React, { Component } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+        <CommentDetail 
+            author={faker.name.firstName()} 
+            time={faker.date.recent().toLocaleString()}
+            body={faker.lorem.sentence()}
+            avatar={faker.image.avatar()}
+            />
+        <CommentDetail 
+            author={faker.name.firstName()} 
+            time={faker.date.recent().toLocaleString()}
+            body={faker.lorem.sentence()}
+            avatar={faker.image.avatar()}
+            />
+        <CommentDetail 
+            author={faker.name.firstName()} 
+            time={faker.date.recent().toLocaleString()}
+            body={faker.lorem.sentence()}
+            avatar={faker.image.avatar()}
+            />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
