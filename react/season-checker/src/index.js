09e9649af37ff7156cec35cacec59af7a5bc45e1
billy <sangyounw@naver.com>
2018-11-20 15:18:19 +0900

    Learn State Season-display

--- react/season-checker/src/index.js
+++ react/season-checker/src/index.js
@@ -1,70 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-
-// const App = () => {
-
-//     window.navigator.geolocation.getCurrentPosition(
-//         position => console.log(position),
-//         error => console.log(error)
-//     )
-
-//     return(
-//         <div>
-//             <SeasonDisplay>
-
-//             </SeasonDisplay>
-//         </div>
-//     );
-// };
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
+  
+//   constructor(props) {
+//     super(props);
 
-        constructor(props) {
-            super(props);
+    state = {
+        lat: null, // 아직모른다. but 들어는 온다.
+        errorMessage: '',
+    };
+//   }
 
-            // state라는 object를 초기화 (예약어)
-            this.state = {
-                lat: null, // 아직 모름
-                errorMessage: '',
+  render() {
+    // 사용자 허용함
+    if (!this.state.errorMessage && this.state.lat) {
+      return (<SeasonDisplay lat={this.state.lat}/>);
+    }
 
-            };
-            window.navigator.geolocation.getCurrentPosition(
-                position => {
-                    this.setState({
-                        lat: position.coords.latitude
-                    });
-                },
-                error => {
-                    this.setState({
-                        errorMessage: error.message
-                    });
-                }
-            );
-        }
+    // 사용자 거부함
+    if (this.state.errorMessage && !this.state.lat) {
+      return (<div>Error: {this.state.errorMessage}</div>);
+    }
+    
+    // 허용/거부 기다리는 중..
+    return(<Spinner message='where are you?'/>);
+  }
 
-        render() {
-            if (this.state.errorMessage && !this.state.lat) {
-                return ( < div > Error: {
-                    this.state.errorMessage
-                         } </div>);
-                }
-
-           if (!this.state.errorMessage && this.state.lat) {
-                return ( < div > Lat: {
-                    this.state.lat
-                        } </div>);
-                }   
-            return ( < div > Loading... </div>)
-                       }
-               }
-
-ReactDOM.render( < App / > , document.querySelector('#root'));
-
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+              this.setState({ lat: position.coords.latitude });
+            },
+            error => { 
+              this.setState({ errorMessage: error.message });
+            }
+          );
+    }
+            
+    componentDidUpdate(){
+        console.log('컴포넌트 update $ re-rendering');
+    }
+}
 
 
 
-                    {/* // 1. 브라우저 jS 파일 불러옴
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+
+                    /* // 1. 브라우저 jS 파일 불러옴
                     // 2. <App/>> 컴포넌트 생ㅇ성
                     // 3. Geolocation API 가 위치정보 받기 시작
                     // 4. React App이 JSX를 리턴하며 html 랜더링
@@ -78,4 +65,4 @@ ReactDOM.render( < App / > , document.querySelector('#root'));
 
                     // lat: O / errorMessage: X => lat 보여준다
                     // lat: x / errorMessage: o => errorMessage 보여줌
-                    // lat: null / errorMessage: null => loading.. 보여줌 */}
+                    // lat: null / errorMessage: null => loading.. 보여줌 */
